5ef602d1ecd9f8d6756e67e3ca0eb633c0e15f20
AIC Web Design <pwm@preservedword.com>
2018-01-11 21:08:04 -0500

    CLIENT-29: Change config file - CONFIG FILE BEING DELETED BY PACK

---
+++ config-example.js
@@ -0,0 +1,20 @@
+var biblesupersearch_config_options = {
+    
+    // Default Bible 
+    //      Automatically selected in first Bible selector
+    "defaultBible": "kjv",                              
+    
+    // Enabled Bibles
+    //      Comment out to enable all Bibles available
+    //      Order indicates order Bibles will appear
+    "enabledBibles": [],                                
+    
+    // Bible SuperSearch skin / interface
+    "interface": "Classic",                             
+    
+    // id of target HTML div tag
+    'target': 'biblesupersearch_container',
+    
+    // URL of Bible SuperSearch API
+    "apiUrl": "https://api.biblesupersearch.com/api",
+};
