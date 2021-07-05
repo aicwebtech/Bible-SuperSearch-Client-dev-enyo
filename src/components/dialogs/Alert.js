612be220c73053e14e7a9c0f1fa879f7f4d9c9e1
AIC Web Tech <pwm@preservedword.com>
2021-07-04 23:28:58 -0400

    Finished Alert dialog?

--- src/components/dialogs/Alert.js
+++ src/components/dialogs/Alert.js
@@ -1,25 +1,27 @@
 var kind = require('enyo/kind');
 var Signal = require('enyo/Signals');
 var Dialog = require('./Dialog');
+var HoverDialog = require('./Hover');
 var i18n = require('../Locale/i18nContent');
 
 module.exports = kind({
     name: 'AlertDialog',
-    // kind: Dialog,
-    width: '200px',
-    height: '130px',
+    kind: Dialog,
+    // kind: HoverDialog,
+    width: '400px',
+    height: '50px',
     classes: 'alert_dialog',
     showing: false,
     alert: '',
     pComponent: null,
 
-    components: [
+    bodyComponents: [
         {kind: Signal, onAlert: 'openAlert', onPositionedAlert: 'openPositionedAlert'},
-        {name: 'Container', allowHtml: true, tag: 'span'}
+        {name: 'AlertContainer', allowHtml: true, tag: 'span', classes: 'alert_dialog_container'}
     ],
 
     bindings: [
-        {from: 'alert', to: '$.Container.content', oneWay: true, transform: function(value, dir) {
+        {from: 'alert', to: '$.AlertContainer.content', oneWay: true, transform: function(value, dir) {
             // console.log('FormatButtons advanced_toggle', value, dir);
             return value;
             // return (value && value != 0 && value != false) ? true : false;
@@ -36,38 +38,51 @@ module.exports = kind({
         this._openAlertHelper(inEvent.alert || 'Alert');
     },
     openPositionedAlert: function(inSender, inEvent) {
+        this._openAlertHelper(inEvent.alert || 'Alert');
+        return;
+
+        // various attempts to get the alert to position near the item clicked, not working
         var oSender = inEvent.inSender,
             oEvent = inEvent.inEvent,
-            x = oEvent.pageX || null,
-            y = oEvent.pageY || null;
+            x = oEvent.clientX || null,
+            y = oEvent.clientY || null;
+
+        x += window.scrollX;
+        y += window.scrollY;
 
         // this.log('oSender', oSender);
-        // this.log('oEvent', oEvent);
+        this.log('oEvent', oEvent);
+        this.log('scrollX', window.scrollX);
+        this.log('scrollY', window.scrollY);
 
-        // if(x & y) {
-        //     this.applyStyle('position', 'absolute');
-        //     this.applyStyle('left', x + 'px');
-        //     this.applyStyle('top', y + 'px');
-        //     // this.applyStyle('width', this.widthMin + 'px');
-        // }
+        if(x & y) {
+            this.applyStyle('position', 'absolute');
+            this.applyStyle('left', x + 'px');
+            this.applyStyle('top', y + 'px');
+            // this.applyStyle('width', this.widthMin + 'px');
+            this.mouseX = x;
+            this.mouseY = y;
+            this._openAlertHelper(inEvent.alert || 'Alert');
+            this.reposition();
+        }
 
         // oSender.set('content', )
-        this.pComponent && this.pComponent.destroy();
+        // this.pComponent && this.pComponent.destroy();
 
-        this.pComponent = oSender.parent.createComponent({
-            classes: 'alert_dialog_container',
-            // tag:'span'
-            components: [
-                {                
-                    tag: 'span',
-                    content: inEvent.alert,
-                    classes: 'alert_dialog',
-                    // style: 'position: absolute; top: -5px'
-                }
-            ]
-        });
+        // this.pComponent = oSender.parent.createComponent({
+        //     classes: 'alert_dialog_container',
+        //     // tag:'span'
+        //     components: [
+        //         {                
+        //             tag: 'span',
+        //             content: inEvent.alert,
+        //             classes: 'alert_dialog',
+        //             // style: 'position: absolute; top: -5px'
+        //         }
+        //     ]
+        // });
 
-        this.pComponent.render();
+        // this.pComponent.render();
 
 
         this._timerHelper();
@@ -86,7 +101,7 @@ module.exports = kind({
         this.timer = window.setTimeout( function() {
             t.set('showing', false);
             t.pComponent && t.pComponent.destroy();
-        }, 1500);
+        }, 400);
 
     }
 });
