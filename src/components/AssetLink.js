9be181c398ee23604b8d7f33a0d7f79282b9e6ec
AIC Web Tech <pwm@preservedword.com>
2022-05-29 16:42:50 -0400

    Work on error message, pt 1

---
+++ src/components/AssetLink.js
@@ -0,0 +1,25 @@
+var Anchor = require('enyo/Anchor');
+var kind = require('enyo/kind');
+
+module.exports = kind({
+    name: 'AssetLink',
+    kind: Anchor,
+
+    published: {
+        relHref: null
+    },
+    create: function() {
+        this.inherited(arguments);
+        this.generateAbsHref();
+    },
+    relHrefChanged: function(was, is) {
+        this.generateAbsHref();
+    },
+    generateAbsHref: function() {
+        if(this.relHref) {
+            var href = this.app.get('rootDir') + '/assets/extras/' + this.relHref;
+            this.log(href);
+            this.set('href', href);
+        }
+    }
+});
