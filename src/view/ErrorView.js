263b89b90433bff79ae21b620fc99c8f38e0ea65
AIC Web Tech <pwm@preservedword.com>
2022-06-23 20:11:00 -0400

    Error msg experimentation, needs work

--- src/view/ErrorView.js
+++ src/view/ErrorView.js
@@ -9,18 +9,23 @@ module.exports = kind({
         message: ''
     },
 
-    bindings: [
-        {from: 'message', to: '$.Message.content'}
-    ],
+    // bindings: [
+    //     {from: 'message', to: '$.Message.content'}
+    // ],
 
     components: [
-        {tag: 'h2', content: 'Bible SuperSearch Init Error'},
-        {name: 'Message', allowHtml: true},
+        {tag: 'h2', content: 'Bible SuperSearch Load Error'},
+        // {name: 'Message', allowHtml: true},
+        {content: 'Bible SuperSearch was unable to load data that it needs to work.'},
+        {content: 'The Bible SuperSearch API may be down or you may be experiencing internet interruptions.  '},
         {tag: 'br'},
         {tag: 'h3', content: 'In the meanwhile, you may download these'},
         {kind: AssetLink, relHref: 'kjv.csv', content: 'CSV Bibles'},
         {tag: 'br'},
         {tag: 'br'},
-        {tag: 'b', content: 'Download the Truth, and Delete it Not!'}
+        {tag: 'b', content: 'Download the Truth, and Delete it Not!'},
+        {tag: 'br'},
+        {tag: 'br'},
+        {tag: 'small', content: 'Webmasters, please see your console for details.'}
     ]
 });
