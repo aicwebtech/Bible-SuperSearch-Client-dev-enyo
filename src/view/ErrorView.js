7ca32d18116ffb66a2b5328059e8245314c1c394
AIC Web Tech <pwm@preservedword.com>
2022-06-30 22:23:34 -0400

    MOre improvements to error msg

--- src/view/ErrorView.js
+++ src/view/ErrorView.js
@@ -13,19 +13,25 @@ module.exports = kind({
     //     {from: 'message', to: '$.Message.content'}
     // ],
 
+    // todo - localize this!!
+
     components: [
-        {tag: 'h2', content: 'Bible SuperSearch Load Error'},
-        // {name: 'Message', allowHtml: true},
-        {content: 'Bible SuperSearch was unable to load data that it needs to work.'},
-        {content: 'The Bible SuperSearch API may be down or you may be experiencing internet interruptions.  '},
-        {tag: 'br'},
-        {tag: 'h3', content: 'In the meanwhile, you may download these'},
-        {kind: AssetLink, relHref: 'kjv.csv', content: 'CSV Bibles'},
-        {tag: 'br'},
-        {tag: 'br'},
-        {tag: 'b', content: 'Download the Truth, and Delete it Not!'},
-        {tag: 'br'},
-        {tag: 'br'},
-        {tag: 'small', content: 'Webmasters, please see your console for details.'}
+        {classes: 'biblesupersearch_content', components: [
+            {_tag: 'h2', classes:'biblesupersearch_error_header', content: 'Bible SuperSearch Load Error'},
+            // {name: 'Message', allowHtml: true},
+            {content: 'Bible SuperSearch was unable to load data that it needs to work.'},
+            {content: 'You may be experiencing internet interruptions.  '},
+            {content: 'Please try refreshing this page.  If the problem persists, please try again later.'},
+            {tag: 'br'},
+            {tag: 'small', content: '(Webmasters, please see your console for error details.)'},
+            {tag: 'br'},
+            {tag: 'br'},
+            {tag: 'h3', components: [
+                {tag: 'span', content: 'In the meanwhile, you may download these '},
+                {kind: AssetLink, relHref: 'kjv.csv', content: 'CSV Bibles'}
+            ]},
+            {tag: 'br'},
+            {tag: 'b', content: 'Download the Truth, and Delete it Not!'}
+        ]}
     ]
 });
