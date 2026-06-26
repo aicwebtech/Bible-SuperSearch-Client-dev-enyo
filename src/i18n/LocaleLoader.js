f2f50b50711263f6e01a7680b4ca2eb2da79d5b2
AIC Web Tech <pwm@preservedword.com>
2026-06-26 10:33:01 -0400

    BSS-273: Adding Luvale language translations

--- src/i18n/LocaleLoader.js
+++ src/i18n/LocaleLoader.js
@@ -20,6 +20,7 @@ module.exports = {
     ja: require('./ja'),
     kn: require('./kn'),
     ko: require('./ko'),
+    lue: require('./lue'), // Luvale
     lv: require('./lv'),
     lt: require('./lt'), 
     mi: require('./mi'), 
